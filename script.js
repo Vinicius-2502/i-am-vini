50bc2fd42795509b0d6e346e64572e8595da14df
Vinicius-2502 <vinicius.nascimento.250209@gmail.com>
2025-03-17 19:09:20 -0300

    Perfil icon update 2.3

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function togglemode() {
   const img = document.querySelector('#profile img')
   if (html.classList.contains('light'))
   {
-    img.setAttribute("src" , "./nova img 2.jpg")
+    img.setAttribute("src", "./ imagem 1.jpg")
   }
-  else{img.setAttribute("src", "./imagem 1.jpg")}
+  else{img.setAttribute("src", "./nova img 2.jpg")}
 }
